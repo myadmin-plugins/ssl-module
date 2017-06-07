0eb299d5c4696df4f7436eef2714a033c6e8137b
Joe Huss <detain@interserver.net>
2017-06-07 13:26:56 -0400

    updated settings

--- src/Plugin.php
+++ src/Plugin.php
@@ -15,6 +15,6 @@ class Plugin {
 
 	public static function Settings(GenericEvent $event) {
 		$settings = $event->getSubject();
-		$settings->add_dropdown_setting('ssl', 'General', 'outofstock_ssl', 'Out Of Stock Ssl', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_WEBHOSTING'), array('0', '1'), array('No', 'Yes', ));
+		$settings->add_dropdown_setting('ssl', 'General', 'outofstock_ssl', 'Out Of Stock Ssl', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_SSL'), array('0', '1'), array('No', 'Yes', ));
 	}
 }
