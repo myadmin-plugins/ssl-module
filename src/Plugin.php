cfd5f4f058c1a70d0c7c4f45193588d9a4caf9a6
Joe Huss <detain@interserver.net>
2017-07-20 00:49:32 -0400

    a bunch of fixs tweaks and iprovements

--- src/Plugin.php
+++ src/Plugin.php
@@ -4,6 +4,11 @@ namespace Detain\MyAdminSsl;
 
 use Symfony\Component\EventDispatcher\GenericEvent;
 
+/**
+ * Class Plugin
+ *
+ * @package Detain\MyAdminSsl
+ */
 class Plugin {
 
 	public static $name = 'SSL Certificates';
@@ -29,10 +34,15 @@ class Plugin {
 		'TITLE_FIELD' => 'ssl_hostname',
 		'PREFIX' => 'ssl'];
 
-
+	/**
+	 * Plugin constructor.
+	 */
 	public function __construct() {
 	}
 
+	/**
+	 * @return array
+	 */
 	public static function getHooks() {
 		return [
 			self::$module.'.load_processing' => [__CLASS__, 'loadProcessing'],
@@ -40,10 +50,16 @@ class Plugin {
 		];
 	}
 
+	/**
+	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+	 */
 	public static function loadProcessing(GenericEvent $event) {
 
 	}
 
+	/**
+	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+	 */
 	public static function getSettings(GenericEvent $event) {
 		$settings = $event->getSubject();
 		$settings->add_dropdown_setting(self::$module, 'General', 'outofstock_ssl', 'Out Of Stock Ssl', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_SSL'), ['0', '1'], ['No', 'Yes']);
